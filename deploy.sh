710a17577147098ffae8c8609615d762927ccc50
Devashish <devashish.86@gmail.com>
2018-03-19 16:27:02 +0530

    checking public key

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,30 @@
 
 set -e
 
-main() {
-	echo "Deploying application"
-	local keys=$(shipctl get_integration_resource_keys app_deploy_key)
-	echo $keys
+readonly PRIVATE_KEY_LOCATION=/tmp/privateKey
 
-	local public_key=$(shipctl get_integration_resource_field app_deploy_key publicKey)
-	echo $public_key
+_extract_private_key() {
+	echo "Extracting private key"
+	local privateKey=$(shipctl get_integration_resource_field app_deploy_key privateKey)
+
+	echo "Writing private key to file"
+	echo $privateKey > $PRIVATE_KEY_LOCATION
+
+	echo "Updating key file permissions"
+	chmod -c 600 $PRIVATE_KEY_LOCATION
+
+	echo "adding private key to ssh-keychain"
+	ssh-add $PRIVATE_KEY_LOCATION
+
+	echo "listing all added key-pairs"
+	ssh-add -L
 }
 
+
+main() {
+	echo "Deploying application"
+  eval $(ssh-agent -s)
+	_extract_private_key
+}
 
 main

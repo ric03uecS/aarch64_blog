65705f31047fcbd36192aba2692a00772e51eaa2
Devashish <devashish.86@gmail.com>
2018-03-19 16:52:13 +0530

    debug

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,38 @@ set -e
 
 readonly PRIVATE_KEY_LOCATION=/tmp/privateKey
 
+_extract_key() {
+  echo "Extracting AWS PEM"
+  echo "-----------------------------------"
+	local pem_key_path=$(shipctl get_resource_meta app_deploy_key)
+  if [ ! -f "integration.json" ]; then
+    echo "No credentials file found at location: $pem_key_path"
+    return 1
+  fi
+
+  cat integration.json | jq -r '.key' > key.pem
+  chmod 600 key.pem
+
+  echo "Completed Extracting AWS PEM"
+  echo "-----------------------------------"
+
+  ssh-add key.pem
+  echo "SSH key added successfully"
+  echo "--------------------------------------"
+
+	echo "Listing all added key-pairs"
+	ssh-add -L
+  echo "--------------------------------------"
+  popd
+}
+
 _extract_private_key() {
 	echo "Extracting private key"
 	local privateKey=$(shipctl get_integration_resource_field app_deploy_key key)
 	#local privateKey=$(shipctl get_integration_resource_keys app_deploy_key)
 
 	echo "Writing private key to file"
-	echo -e $privateKey > $PRIVATE_KEY_LOCATION
+	echo $privateKey > $PRIVATE_KEY_LOCATION
 
 	cat $PRIVATE_KEY_LOCATION
 
@@ -28,7 +53,8 @@ _extract_private_key() {
 main() {
 	echo "Deploying application"
   eval $(ssh-agent -s)
-	_extract_private_key
+	#_extract_private_key
+	_extract_key
 }
 
 main

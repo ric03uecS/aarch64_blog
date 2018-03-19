b324e4149a8e6eaee914385467bc0eba0c9512d7
Devashish <devashish.86@gmail.com>
2018-03-19 16:20:55 +0530

    checking public key

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ main() {
 	local keys=$(shipctl get_integration_resource_keys app_deploy_key)
 	echo $keys
 
+	local public_key=$(shipctl get_integration_resource_field app_deploy_key publicKey)
+	echo $public_key
 }
 
 

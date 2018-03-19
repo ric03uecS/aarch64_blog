c7b5ae775fc59864e18f41709bfcc8f552489903
Devashish <devashish.86@gmail.com>
2018-03-19 13:59:21 +0530

    updating blog helpers

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'demoBlog.. it works' });
+  res.render('index',
+    {
+      title: 'aarch64 blog',
+      release: 'release version from global env',
+      version: 'output of uname'
+    }
+  );
 });
 
 module.exports = router;
